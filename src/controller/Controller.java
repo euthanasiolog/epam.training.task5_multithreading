9e4e2e23101156fdd405e8072d53e65f8ebf403b
euthanasiolog <piatryxa@gmail.com>
2018-08-25 21:02:02 +0300

    fixed

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,6 +3,7 @@ package controller;
 import entity.Port;
 import entity.Ship;
 import logic.PortManager;
+import logic.StorageManager;
 import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
@@ -28,6 +29,9 @@ public class Controller {
             callable.add(new Ship((int) (Math.random()*20+2), true, portManager));
         }
         try {
+            StorageManager storageManager = new StorageManager(PORT);
+            storageManager.setDaemon(true);
+            executorService.submit(storageManager);
             executorService.invokeAll(callable);
             executorService.shutdown();
         } catch (InterruptedException e) {
--- src/logic/StorageManager.java
+++ src/logic/StorageManager.java
@@ -1,18 +1,35 @@
 package logic;
 
 import entity.Port;
-import entity.Ship;
 
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.Callable;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by piatr on 25.08.18.
  */
-public class StorageManager {
-    private Semaphore semaphore;
-    private ReentrantLock lock;
-    private Ship ship;
+public class StorageManager extends Thread {
+    private ReentrantLock lock = new ReentrantLock();
     private Port port;
 
+    public StorageManager (Port port) {
+        this.port = port;
+
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+         lock.lock();
+         if (port.getStorage().get()<(Port.getStorageCapacity()/5)) {
+             port.setStorage(port.getStorage().get()+(Port.getStorageCapacity()/2));
+         }
+         lock.unlock();
+         lock.lock();
+         if (port.getStorage().get()>(Port.getStorageCapacity()/5)*4) {
+             port.setStorage(Port.getStorageCapacity()/2);
+         }
+         lock.unlock();
+        }
+    }
 }
